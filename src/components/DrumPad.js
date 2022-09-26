cc398914914bd9370d08c6d6d50958833769e029
Gabriel Baldock <gabrielbaldock@gmail.com>
2022-09-26 23:16:33 +0100

    Implemented keystroke listener

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 class DrumPad extends React.Component {
+  componentDidMount() {
+    document.addEventListener(
+      "keydown",
+      (event) => {
+        var name = event.key;
+        if (event.key.toUpperCase() === this.props.sound.key) this.clickedPad();
+      },
+      false
+    );
+  }
+
   clickedPad = () => {
     document.getElementById(this.props.sound.key).play();
   };
