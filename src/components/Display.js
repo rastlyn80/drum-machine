6f8d921f289bbcf0c55a6defdb0901d8dad395b0
Gabriel Baldock <gabrielbaldock@gmail.com>
2022-09-26 22:57:23 +0100

    Introduced credits in the right panel

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Credits from "./Credits";
 
 class Display extends React.Component {
   render() {
-    return <div id="display">DISPLAY</div>;
+    return (
+      <div className="right-panel">
+        <div id="display">DISPLAY</div>
+        <Credits />
+      </div>
+    );
   }
 }
 
